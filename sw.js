79801654c0cc766b1fdb929cff3c7290c13e6291
ftima13 <utp0156952@alumno.utpuebla.edu.mx>
2025-09-27 22:07:54 -0600

    Nuevo

--- sw.js
+++ sw.js
@@ -16,5 +16,34 @@ self.addEventListener('fetch',
     }
   }
 );
+// Nombre del caché
+var cacheName = 'helloWorld';
+
+// Evento de instalación del SW: cachea archivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        '/pwa2/index.html',
+        '/pwa2/lib1.js',
+        '/pwa2/lib2.js',
+        '/pwa2/hola.jpg',
+        '/pwa2/unicorn.jpg',
+        '/pwa2/utp.png',
+      ]))
+  );
+});
+
+// Evento fetch: responde desde la caché si existe
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response; // Devuelve desde caché
+      }
+      return fetch(event.request); // Si no está en caché, va a la red
+    })
+  );
+});
 
 
